77e7b3d98a63dcf3fdcb2f38a4a4875e08daf61a
Artem Nemtsov <nemcov_a@mail.ru>
2018-10-02 14:27:39 +0500

    EmptyProjectCreated

---
+++ TaskForLorena(WorkWithBD_SQL_Lite)/TaskForLorena(WorkWithBD_SQL_Lite)/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+
+namespace TaskForLorena_WorkWithBD_SQL_Lite_
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
