b8bb7c83db173636fdd378b9a771a3daeb85521e
Artem Nemtsov <nemcov_a@mail.ru>
2018-10-02 18:37:18 +0500

    added element of Tollbox

--- TaskForLorena(WorkWithBD_SQL_Lite)/TaskForLorena(WorkWithBD_SQL_Lite)/MainWindow.xaml.cs
+++ TaskForLorena(WorkWithBD_SQL_Lite)/TaskForLorena(WorkWithBD_SQL_Lite)/MainWindow.xaml.cs
@@ -12,13 +12,43 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace TaskForLorena_WorkWithBD_SQL_Lite_
+namespace TaskforLorena_work_with_DBSQLite_
 {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+            Сhoice.Items.Add("ywafw");
+            Сhoice.Items.Add("twafw");
+            Сhoice.Items.Add("rwafw");
+            Сhoice.Items.Add("ewafw");
+            Сhoice.Items.Add("wwafw");
+            Сhoice.Items.Add("qwafw");
+
+        }
+
+        private void comboBox1_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+        }
+
+        private void textBox1_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            textBlock1.Text = "qweerty";
+
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
