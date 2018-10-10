1dd304f2be588d963fbb39df515e132af3a44f70
ArtemNemtsov <31465417+ArtemNemtsov@users.noreply.github.com>
2018-10-10 09:38:00 +0500

    Changed comboBox on TreeView

--- TaskForLorena(WorkWithBD_SQL_Lite)/TaskForLorena(WorkWithBD_SQL_Lite)/MainWindow.xaml.cs
+++ TaskForLorena(WorkWithBD_SQL_Lite)/TaskForLorena(WorkWithBD_SQL_Lite)/MainWindow.xaml.cs
@@ -4,13 +4,6 @@ using System.Linq;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.Data.SQLite;
 using System.IO;
 
@@ -23,52 +16,44 @@ namespace Lorena
         public MainWindow()
         {
             InitializeComponent();
-            
-            
         }
-
-        private static List<IDepartment> flattenDepartments(List<IDepartment> deps)
+                                                                                     
+        private static List<IDepartment> flattenDepartments(List<IDepartment> deps)      //метод вернет все оффисы(от родительских к дочерним) 
         {
-            List<IDepartment> result = new List<IDepartment>();        //создаем List
-            foreach (var dep in deps) // для каждого офисса из полученного списка оффисов делаем :
+            List<IDepartment> result = new List<IDepartment>();                  //создаем List
+            foreach (var dep in deps)                                           // для каждого офисса из полученного списка оффисов делаем :
             {
-                result.Add(dep);            // 1) каждый офиис добавляем в лист Result
+                result.Add(dep);                                                // 1) каждый офиис добавляем в лист Result
                 var children = flattenDepartments(dep.GetChildDepartments());   // получаем дочку и ее тоже передаем в аргументы flattenDepartments(дочка)
-                foreach (var child in children)  //3) для каждого дочернего оффиса : 
+                foreach (var child in children)                                 //3) для каждого дочернего оффиса : 
                 {
-                    result.Add(child);     //Добавляем в лист Result дочку
+                    result.Add(child);                                           //Добавляем в лист Result дочку
                 }
             }
-            return result;    // возвращаем лист result
+            return result;                                                       // возвращаем лист result
         }
 
-        static double CalcTotalDiscount(IDepartment dep)
+        static double CalcTotalDiscount(IDepartment dep)                          //вычисляем общую (родительскую) скидку для конкретного dep
         {
             double discount = dep.Discount;
-            if (dep.IsDependent && dep.Parent != null && dep.Parent.Id > 0)
-            { 
-                discount += CalcTotalDiscount(dep.Parent);
+            if (dep.IsDependent && dep.Parent != null && dep.Parent.Id > 0)     // если есть родительи его ID !=0, то 
+            {
+                discount += CalcTotalDiscount(dep.Parent);                      // скидка = скидка + (вычисляем скидку(родителя))
             }
             return discount;
         }
 
-        double CalcDiscountedPrice(IDepartment dep, double price, bool logToDataBase = false)
+        double CalcDiscountedPrice(IDepartment dep, double price, bool logToDataBase = false)    // вычисляем цену для конкретного dep по формуле
         {
-            double totalDiscount = CalcTotalDiscount(dep);
-            double discountedPrice = price - price * (totalDiscount / 100.0);
+            double totalDiscount = CalcTotalDiscount(dep);                         //вычисляем родительскую скидку
+            double discountedPrice = price - price * (totalDiscount / 100.0);      //скидка для нашего dep
 
-            if (logToDataBase)
-            {
+            if (logToDataBase)                                                     //если (нажата клавиша "Записать в БД"), то
+            {   
                 double parentDiscount = totalDiscount - dep.Discount;
-                storage.LogResult(dep, price, parentDiscount, discountedPrice);
+                storage.LogResult(dep, price, parentDiscount, discountedPrice);    //записываем результат в БД
             }
             return discountedPrice;
-        }
-
-   
-        private void button1_Click(object sender, RoutedEventArgs e)
-        {
-            RecalcResult(true);
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
@@ -77,67 +62,75 @@ namespace Lorena
             string BDFileName = "TestBDForLorena.db";
             string cwd = System.IO.Directory.GetCurrentDirectory();
             string fullPathToBD = "";
-            fullPathToBD = System.IO.Path.Combine(connectDBPath, BDFileName);  //комбинируем полный путь   к БД
+            fullPathToBD = System.IO.Path.Combine(connectDBPath, BDFileName);        //комбинируем полный путь   к БД
             storage = new DBStorage(fullPathToBD);
-            
-            storage.CreateRequiredTableIsNotExist();
-           
-            if (storage.TableIsEmpty("TestTask"))  // Заполняем если в таблице нет записей
+
+            storage.CreateRequiredTableIsNotExist();                                 //создаем таблицу, если онане существует...
+
+            if (storage.TableIsEmpty("TestTask"))                                    // Если таблица пуста, заполняем ее данными ниже...
             {
-                var IdMiass = storage.CreateDepartment("Миасс", 4, false, "");        //заполняем таблицу в БД
+                var IdMiass = storage.CreateDepartment("Миасс", 4, false, "");              //заполняем таблицу в БД
                 var IdAmelia = storage.CreateDepartment("Амелия", 5, true, "", IdMiass);
-                var IdTest1 = storage.CreateDepartment("Тест1", 2, true, "", IdAmelia);               
+                var IdTest1 = storage.CreateDepartment("Тест1", 2, true, "", IdAmelia);
                 var IdTest2 = storage.CreateDepartment("Тест2", 0, true, "", IdMiass);
                 var IdKurgan = storage.CreateDepartment("Курган", 11, false, "");
             }
+            var mainDeps = storage.GetMainDepartments();                             // получаем главные офиссы
+            var allDepartments = flattenDepartments(mainDeps);                       // получаем  в list<Departament> все офисы
 
-         
-            var mainDeps = storage.GetMainDepartments();    // получаем главные офиссы
-            var allDepartments = flattenDepartments(mainDeps);  // получаем все офисы в list<Departament>
-
-            cbDepartment.ItemsSource = allDepartments;
-            cbDepartment.DisplayMemberPath = "Name";
-            cbDepartment.SelectedIndex = 0;
+            AppendChildrenRecursively(treebase, mainDeps);                          
         }
 
-        private void RecalcResult(bool logToDatabase = false)
+        void AppendChildrenRecursively(ItemsControl treebase, List<IDepartment> ChildDeparts)       //метод показывает список магазинов в дереве TreeView
         {
-            IDepartment selectedDepartment = (IDepartment)cbDepartment.SelectedItem;
-            
-            double price;
-            if (Double.TryParse(textBox1.Text, out price) && selectedDepartment != null)
+            foreach (var dep in ChildDeparts)                  
             {
-                double discountedPrice = CalcDiscountedPrice(selectedDepartment, price, logToDatabase);
-                tbDiscountedPrice.Text = discountedPrice.ToString();
+                TreeViewItem rootItem = new TreeViewItem();
+                rootItem.Tag = dep;
+                rootItem.Header = dep.Name;
+                treebase.Items.Add(rootItem);
+                AppendChildrenRecursively(rootItem, dep.GetChildDepartments());
+            }
+        }
 
-                if (logToDatabase)
+        private void RecalcResult(bool logtodatabase = false)                              //вычисляем результат, по умолчанию logToDatabase = false (записи в БД не делаем)
+        {
+            ItemsControl item = (TreeViewItem)treebase.SelectedItem;                       // Свойство SelectedItem реализованное для TreeViewItem возвращает тип Object.
+
+            IDepartment selecteddepartment = (IDepartment)item.Tag;
+
+            double price;
+            if (double.TryParse(textBox1.Text, out price) && selecteddepartment != null)
+            {
+                double discountedprice = CalcDiscountedPrice(selecteddepartment, price, logtodatabase);
+                tbDiscountedPrice.Text = discountedprice.ToString();
+
+                if (logtodatabase)
                 {
-                    MessageBox.Show("Данные записаны", "Результат", MessageBoxButton.OK, MessageBoxImage.Information);
+                    MessageBox.Show("данные записаны", "результат", MessageBoxButton.OK, MessageBoxImage.Information);
                 }
 
             }
-            else if (logToDatabase)
+            else if (logtodatabase)
             {
-                MessageBox.Show("Невозможно записать данные в БД. Введите цену.", "Ошибка!", MessageBoxButton.OK, MessageBoxImage.Error);
+                MessageBox.Show("невозможно записать данные в бд. введите цену.", "ошибка!", MessageBoxButton.OK, MessageBoxImage.Error);
             }
         }
 
-        private void comboBox1_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        private void BtnWriteDataClick(object sender, RoutedEventArgs e)
         {
-            RecalcResult();
+            RecalcResult(true);                  //записать в БД - true
         }
 
         private void textBox1_TextChanged(object sender, TextChangedEventArgs e)
         {
             RecalcResult();
         }
+
+        private void treebase_SelectedItemChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+        {
+            RecalcResult();
+        }
     }
 }
 
-/*
-  List<string> ShopsName = BDStorage.GetShops(); // показываем список магазинов
-  foreach (string shop in ShopsName)
-  {
-      Сhoice.Items.Add(shop);
-  }
-  */
